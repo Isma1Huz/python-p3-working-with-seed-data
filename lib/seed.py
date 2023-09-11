1e1d1925ecf9870ca48b9664ca7ebc3522c2a252
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-09-11 08:37:03 +0300

    Done

--- lib/seed.py
+++ lib/seed.py
@@ -7,6 +7,16 @@ from sqlalchemy.orm import sessionmaker
 
 from models import Game
 
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
 fake = Faker()
 
 if __name__ == '__main__':
